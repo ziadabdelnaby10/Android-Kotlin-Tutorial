825062bb681b0c0e39451d9df23901e3f170a880
Ziad Abdelnaby <ziadabdelnaby46@gmail.com>
2021-08-13 17:01:32 +0200

    updating the fragment constructor

--- NavigationComponentProject/app/src/main/java/com/example/myapplication/ViewBalanceFragment.kt
+++ NavigationComponentProject/app/src/main/java/com/example/myapplication/ViewBalanceFragment.kt
@@ -6,16 +6,5 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ViewBalanceFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_balance, container, false)
-    }
-
-
+class ViewBalanceFragment : Fragment(R.layout.fragment_view_balance) {
 }
--- NavigationComponentProject/app/src/main/java/com/example/myapplication/ViewTransactionFragment.kt
+++ NavigationComponentProject/app/src/main/java/com/example/myapplication/ViewTransactionFragment.kt
@@ -6,16 +6,5 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ViewTransactionFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_transaction, container, false)
-    }
-
-
+class ViewTransactionFragment : Fragment(R.layout.fragment_view_transaction) {
 }
